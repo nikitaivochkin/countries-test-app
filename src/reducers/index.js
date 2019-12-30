2492791dd2826be5e33a5dc04d8822f227ac0a41
nikita_ivochkin <nikita.ivochkin@icloud.com>
2019-12-30 21:32:24 +0300

    add isOpenEl to uiState

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import { combineReducers } from 'redux';
-import { handleActions, handleAction } from 'redux-actions';
+import { handleActions } from 'redux-actions';
 import { reducer as formReducer } from 'redux-form';
 import * as actions from '../actions'
 
@@ -36,9 +36,31 @@ const elements = handleActions({
 }, { byId: {}, allIds: [], currentSelector: 'Country' });
 
 const uiState = handleActions({
-    [actions.updateSelector](_state, { payload: { currentSelector } }) {
+    [actions.updateSelector](state, { payload: { currentSelector } }) {
+        const { isOpenEl } = state;
         return {
             currentSelector: currentSelector,
+            isOpenEl,
+        };
+    },
+    [actions.newSearchSuccess](state, { payload: { data } }) {
+        const { currentSelecrot } = state;
+        const setIdAndStatusForEachEl = data.map((e) => {
+            const setId = _.set(e, 'id', _.uniqueId());
+            const setStatus = _.set(setId, 'status', 'close');
+            return _.pick(setStatus, ['id', 'status']);
+        });
+        return {
+            currentSelecrot,
+            isOpenEl: _.mapKeys({ ...setIdAndStatusForEachEl }, (key) => key.id),
+        }
+    },
+    [actions.openElement](state, { payload: { isOpenEl: { id } } }) {
+        const { currentSelecrot, isOpenEl } = state;
+        const f = (s) => (s === 'close' ? 'open' : 'close');
+        return {
+            currentSelecrot,
+            isOpenEl: _.update(isOpenEl, `${id}.status`, f),
         };
     },
 }, {});
