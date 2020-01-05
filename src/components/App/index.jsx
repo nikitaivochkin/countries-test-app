c5e106d296b653775df6e45b8a764d34472b28e6
nikita_ivochkin <nikita.ivochkin@icloud.com>
2020-01-05 17:16:15 +0300

    add App

---
+++ src/components/App/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import MainInput from '../MainInput';
+import ReceivedData from '../ReceivedData';
+
+const App = () => (
+  <>
+    <MainInput />
+    <ReceivedData />
+  </>
+);
+export default App;
