251494fd03b08df28e14bae6f1abb5cfa6c0c32b
nikita_ivochkin <nikita.ivochkin@icloud.com>
2020-01-08 19:44:43 +0300

    add options

---
+++ src/assets/options.js
@@ -0,0 +1,19 @@
+export const regions = [['disabled', 'Make choose'],
+    ['africa', 'Africa'],
+    ['americas', 'Americas'],
+    ['europe', 'Europe'],
+    ['oceania', 'Oceania']];
+
+export const regionalBlocs = [['disabled', 'Make choose'],
+    ['European Union', 'EU (European Union)'],
+    ['European Free Trade Association', 'EFTA (European Free Trade Association)'],
+    ['Caribbean Community', 'CARICOM (Caribbean Community)'],
+    ['Pacific Alliance', 'PA (Pacific Alliance)'],
+    ['African Union', 'AU (African Union)'],
+    ['Union of South American Nations', 'USAN (Union of South American Nations)'],
+    ['Eurasian Economic Union', 'EEU (Eurasian Economic Union)'],
+    ['Arab League', 'AL (Arab League)'],
+    ['Association of Southeast Asian Nations', 'ASEAN (Association of Southeast Asian Nations)'],
+    ['Central European Free Trade Agreement', 'CEFTA (Central European Free Trade Agreement)'],
+    ['North American Free Trade Agreement', 'NAFTA (North American Free Trade Agreement)'],
+    ['South Asian Association for Regional Cooperation', 'SAARC (South Asian Association for Regional Cooperation)']];
--- src/components/MainInput/index.jsx
+++ src/components/MainInput/index.jsx
@@ -1,11 +1,14 @@
 /* eslint-disable react/prefer-stateless-function */
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import './main.sass';
+import { regions, regionalBlocs } from '../../assets/options.js';
 
 const mapStateToPorps = (state) => {
   const props = {
+    text: state.text,
     elements: state.elements,
     uiState: state.uiState,
   };
@@ -26,44 +29,20 @@ class MainInput extends React.Component {
 
   handleAutocompliteBySelector = ({ target: { value } }) => {
     const { updateText, findElementBySelector } = this.props;
-    updateText({ text: value });
+    updateText({ value });
     findElementBySelector({ inputValue: value });
   }
 
   render() {
-    const { elements: { selector } } = this.props;
+    const { elements: { selector }, text } = this.props;
 
     const getOptionstDependsOnSelector = {
-      region: (
-        <div className="search-bar-row row3">
-          <select onChange={this.handleAutocompliteBySelector} className="search-bar-select" name="selector" component="select" required placeholder="Enter some text">
-            <option className="search-bar-option" value="disabled">Make choose</option>
-            <option className="search-bar-option" value="africa">Africa</option>
-            <option className="search-bar-option" value="americas">Americas</option>
-            <option className="search-bar-option" value="europe" defaultValue>Europe</option>
-            <option className="search-bar-option" value="oceania">Oceania</option>
-          </select>
-        </div>
-      ),
-      regionalBlocs: (
-        <div className="search-bar-row row3">
-          <select onChange={this.handleAutocompliteBySelector} className="search-bar-select" name="selector" component="select" required placeholder="Enter some text">
-            <option className="search-bar-option" value="disabled">Make choose</option>
-            <option className="search-bar-option" value="European Union">EU (European Union)</option>
-            <option className="search-bar-option" value="European Free Trade Association">EFTA (European Free Trade Association)</option>
-            <option className="search-bar-option" value="Caribbean Community">CARICOM (Caribbean Community)</option>
-            <option className="search-bar-option" value="Pacific Alliance">PA (Pacific Alliance)</option>
-            <option className="search-bar-option" value="African Union">AU (African Union)</option>
-            <option className="search-bar-option" value="Union of South American Nations">USAN (Union of South American Nations)</option>
-            <option className="search-bar-option" value="Eurasian Economic Union">EEU (Eurasian Economic Union)</option>
-            <option className="search-bar-option" value="Arab League">AL (Arab League)</option>
-            <option className="search-bar-option" value="Association of Southeast Asian Nations">ASEAN (Association of Southeast Asian Nations)</option>
-            <option className="search-bar-option" value="Central European Free Trade Agreement">CEFTA (Central European Free Trade Agreement)</option>
-            <option className="search-bar-option" value="North American Free Trade Agreement">NAFTA (North American Free Trade Agreement)</option>
-            <option className="search-bar-option" value="South Asian Association for Regional Cooperation">SAARC (South Asian Association for Regional Cooperation)</option>
-          </select>
-        </div>
-      ),
+      region: regions.map(([v, t]) => (
+        <option key={_.uniqueId()} className="search-bar-option" value={v}>{t}</option>
+      )),
+      regionalBlocs: regionalBlocs.map(([v, t]) => (
+        <option key={_.uniqueId()} className="search-bar-option" value={v}>{t}</option>
+      )),
     };
 
     return (
@@ -72,10 +51,16 @@ class MainInput extends React.Component {
           <div className="row">
             <div className="search-bar-row row1">
               {getOptionstDependsOnSelector[selector] ? null : (
-                <input onChange={this.handleAutocompliteBySelector} disabled={selector === 'disabled'} className="search-bar-input" name="text" component="input" required type="text" placeholder="Enter some text" />
+                <input onChange={this.handleAutocompliteBySelector} disabled={selector === 'disabled'} value={text} className="search-bar-input" name="text" component="input" required type="text" placeholder="Enter some text" />
               )}
             </div>
-            {getOptionstDependsOnSelector[selector] === undefined ? null : getOptionstDependsOnSelector[selector]}
+            {getOptionstDependsOnSelector[selector] === undefined ? null : (
+              <div className="search-bar-row row3">
+                <select onChange={this.handleAutocompliteBySelector} className="search-bar-select" name="selector" component="select" required>
+                  {getOptionstDependsOnSelector[selector]}
+                </select>
+              </div>
+            )}
             <div className="search-bar-row row2">
               <select onChange={this.handleUpdateCurrentSelector} className="search-bar-select" name="selector" component="select" required placeholder="Enter some text">
                 <option className="search-bar-option" value="disabled">Make choose</option>
@@ -88,6 +73,9 @@ class MainInput extends React.Component {
             </div>
           </div>
         </form>
+        {getOptionstDependsOnSelector[selector] ? (
+          <h2 className="search-bar-title">{_.capitalize(text)}</h2>
+        ) : null }
       </div>
     );
   }
